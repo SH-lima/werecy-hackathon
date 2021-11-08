8eb781db99ebf1accafc8323c916c0c00f95ae51
etienne-kher <etienne.kher@laplateforme.io>
2021-11-08 22:50:21 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <link href="styles/header.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <?=include('page/header.php')?>
+</body>
+</html>
---
+++ readmepls.txt
@@ -0,0 +1,5 @@
+message d'Etienne:
+voila la structure de base, on peut encore la faire evoluer si vous le souhaitez
+
+vous pouvez deja inclure le header dans vos page 
+
